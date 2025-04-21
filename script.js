c09e75036f777c04bc2f8ae1b2268e2fde63c15a
Julila Mpesha <julilampesha@gmail.com>
2025-04-21 17:25:45 -0400

    Methods have been added to get the total value of stock products nad return a string.

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ class ProductProperties {
       this.price = price;
       this.quantity = quantity;
     }
-}
+  
+    getTotalValue() {
+      return this.price * this.quantity;
+    }
+  
+    toString() {
+      return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
+    }
+  }
+  
