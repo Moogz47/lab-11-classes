92eb8a44a30dc06207db45425c364cc44e753dd5
Julila Mpesha <julilampesha@gmail.com>
2025-04-21 18:00:41 -0400

    addProduct, getInventoryValue, and findProductByName methods have been added.

--- script.js
+++ script.js
@@ -42,4 +42,16 @@ class Store {
   constructor() {
     this.inventory = [];
   }
+  
+  addProduct(product) {
+    this.inventory.push(product);
+  }
+
+  getInventoryValue() {
+    return this.inventory.reduce((total, product) => total + product.getTotalValue(), 0);
+  }
+
+  findProductByName(name) {
+    return this.inventory.find(product => product.name === name) || null;
+  }
 }
